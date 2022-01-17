073e261f0d7cb6873535ce509d54bb3cfa07c9c1
mdiktushar <md.tushar1804@gmail.com>
2022-01-18 00:45:33 +0600

    navbar

--- resources/views/Assets/links.blade.php
+++ resources/views/Assets/links.blade.php
@@ -6,8 +6,8 @@
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 
-<link rel="stylesheet" href="Assets/css/style-1.css">
-
 <!-- jquery -->
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
     crossorigin="anonymous"></script>
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -5,11 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('Assets.links')
+    <link rel="stylesheet" href="Assets/css/style-2.css">
     <title>Class Management System</title>
 </head>
 
 <body>
-    <h1>Student Dashboard</h1>
+    <div class="sidebar">
+        <a class="active" href="#home">Home</a>
+        <a href="#">Profile</a>
+        <a href="#">Result</a>
+        <a href="#">About</a>
+    </div>
+
+    <div class="content">
+        <h1>Hello World</h1>
+    </div>
+
 </body>
 
 </html>
